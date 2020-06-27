0adb1bc2e2fb07be1e950a3dce882760e26209dd
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 19:26:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
         }
      }
   programmer_hash[:dennis_ritchie] [:languages] = "C"
-  programmer_hash
+  programmer_hash[:dennis_ritchie] [:languages]
 end
 
 def adding_matz

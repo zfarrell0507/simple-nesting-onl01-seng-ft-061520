6b37c5ee0dfcd37eb99c27c78869ff8d0f1cba44
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 19:23:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie] [:languages]
+  programmer_hash[:dennis_ritchie] [:languages] 
   programmer_hash
 end
 
